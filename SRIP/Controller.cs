d6a39b3017b5e5df02da78e61827049c7ab61e70
Kaveira <101720324+Kaveira85@users.noreply.github.com>
2023-05-02 19:47:57 +0100

    Add files via upload

---
+++ SRIP/Controller.cs
@@ -0,0 +1,16 @@
+﻿namespace SRIP_EventosDelegados {
+    class Controller {
+
+        Model model;
+        View view;
+
+        public Controller() {
+            model = new Model();
+            view = new View();
+        }
+        public void IniciarPrograma() {
+            Vista vista = new Vista();
+            vista.Iniciar();
+        }
+    }
+}
---
+++ SRIP/SRIP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRIP_EventosDelegados {
+    
+    class SRIP {
+
+        static void Main(string[] args) {
+            Controller controller = new Controller();
+            controller.IniciarPrograma();
+        }
+    };
+}
