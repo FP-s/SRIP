c9970afb66763d5c2e97349b8a8d01dfebfe05d0
Kaveira <tiagofbbento@hotmail.com>
2023-05-03 18:07:46 +0100

    031800MAI23

--- SRIP/Model.cs
+++ SRIP/Model.cs
@@ -4,5 +4,16 @@ namespace SRIP_EventosDelegados {
 
     class Model {
 
+<<<<<<< HEAD
+        public bool VerificarLogin(string login)
+        {
+            //se login for válido retorna true
+            //se não pede nova verificação (falta implementar)
+
+            return true;
+        }
+
+=======
+>>>>>>> 0584e0c5045c668576d437c18d49ae087064ef58
     }
 }
--- SRIP/View.cs
+++ SRIP/View.cs
@@ -4,5 +4,17 @@ namespace SRIP_EventosDelegados {
 
     class View {
 
+<<<<<<< HEAD
+        public void ApresentarLogin()
+        {
+            Console.WriteLine("Apresentar interface de login");
+        }
+        public void ApresentarInterface()
+        {
+            Console.WriteLine("Apresentar interface do programa");
+        }
+
+=======
+>>>>>>> 0584e0c5045c668576d437c18d49ae087064ef58
     }
 }
