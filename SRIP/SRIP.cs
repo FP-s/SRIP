a010e13bb7964816efc6bb116b785044f8dfd6c6
Kaveira <tiagofbbento@hotmail.com>
2023-05-03 18:12:01 +0100

    031800MAI23

--- SRIP/SRIP.cs
+++ SRIP/SRIP.cs
@@ -7,6 +7,7 @@ namespace SRIP_EventosDelegados {
         //Ponto de entrada principal do programa
         static void Main(string[] args) {
 
+            //iniciar componente controller
             Controller controller = new Controller();
             controller.IniciarPrograma();
         }
